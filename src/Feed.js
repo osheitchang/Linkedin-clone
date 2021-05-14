562b3d7b9e6ae4855265f26894f69ab5055a0f63
Osheit <chang.osheit@gmail.com>
2021-05-14 11:20:17 -0400

    new

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
@@ -6,9 +6,10 @@ import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import InputOption from "./InputOption";
-import Post from './Post'
+import Post from "./Post";
 
 function Feed() {
+  const [post, setPosts] = useState([]);
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -30,8 +31,16 @@ function Feed() {
           />
         </div>
       </div>
-      <div >
-        <Post name="Osheit" description="Hello from the other side" message="it works" photoUrl="ds" />
+      <div>
+      {post.map((post) => (
+        <Post />
+      ))}
+        <Post
+          name="Osheit"
+          description="Hello from the other side"
+          message="it works"
+          photoUrl="ds"
+        />
       </div>
     </div>
   );
